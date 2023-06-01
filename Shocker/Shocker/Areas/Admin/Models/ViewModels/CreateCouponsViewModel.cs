8e72fbcf86fba16dbea767ef2c11529f6ef92fc9
Waylen <huangyenrong@gmail.com>
2023-06-01 21:54:59 +0800

    Admin還原

---
+++ Shocker/Shocker/Areas/Admin/Models/ViewModels/CreateCouponsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Shocker.Areas.Admin.Models.ViewModels
+{
+    public class CreateCouponsViewModel
+    {
+        public DateTime ExpirationDate { get; set; }
+        public string HolderAccount { get; set; }
+        public int ProductCategoryId { get; set; }
+        //[Required(ErrorMessage="請輸入折扣小於1")]
+        public decimal Discount { get; set; }
+        public string Status { get; set; }
+        public string PublisherAccount { get; set; }
+        public int Amount { get; set; }
+    }
+}
