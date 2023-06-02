2209e5aa68d3024ca2639675f4d9a20380da9723
y <opo91did@gmail.com>
2023-06-02 16:40:52 +0800

    seller

--- Shocker/Shocker/Models/ViewModels/ReplyModel.cs
+++ Shocker/Shocker/Models/ViewModels/ReplyModel.cs
@@ -4,6 +4,7 @@
     {
         public int ProductId { get; set; }
         public int OrderId { get; set; }
+        public string SellerAccount { get; set; }
         public string? Reply { get; set; }
     }
 }
