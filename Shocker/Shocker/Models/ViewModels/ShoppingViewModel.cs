984438eba9d0534d12f995a2ca6af5db1b9cdee9
y <opo91did@gmail.com>
2023-06-03 18:17:54 +0800

    Shopping Cart

--- Shocker/Shocker/Models/ViewModels/ShoppingViewModel.cs
+++ Shocker/Shocker/Models/ViewModels/ShoppingViewModel.cs
@@ -2,9 +2,8 @@
 {
     public class ShoppingViewModel
     {
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public int Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
+        public string BuyerAccount { get; set; }
+        public List<int> ProductId { get; set; }
+        public List<int> Quantity { get; set; }
     }
 }
