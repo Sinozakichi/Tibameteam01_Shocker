535712dbd3b0ef9e67478bcd10d341e1eab945d9
CHOU <chouchouchou@kimo.com>
2023-06-06 21:01:05 +0800

    修改部分欄位，改成可為null

--- Shocker/Shocker/Models/ViewModels/PaymentAsyncViewModel.cs
+++ Shocker/Shocker/Models/ViewModels/PaymentAsyncViewModel.cs
@@ -5,7 +5,7 @@
 		public string payType { get; set; }
 		public int amount { get; set; }
 		public string Email { get; set; }
-		public string ItemDesc { get; set; }
-		public string OrderComment { get; set; }
+		public string? ItemDesc { get; set; }
+		public string? OrderComment { get; set; }
 	}
 }
