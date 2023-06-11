e2d93e324acb62e05959ea48abf2327737a6d69d
CHOU <chouchouchou@kimo.com>
2023-06-11 14:11:54 +0800

    修正部分bug

--- Shocker/Shocker/Models/ViewModels/PaymentAsyncViewModel.cs
+++ Shocker/Shocker/Models/ViewModels/PaymentAsyncViewModel.cs
@@ -6,7 +6,7 @@
 		public string PayType { get; set; }
 		public int Amt { get; set; }
 		public string Email { get; set; }
-		//public string? ItemDesc { get; set; }
-		//public string? OrderComment { get; set; }
+		public string? ItemDesc { get; set; }
+		public string? OrderComment { get; set; }
 	}
 }
